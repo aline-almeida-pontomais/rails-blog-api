3eb95bd45c22c73bf89b3eda28925ed935cd8277
aline-almeida-pontomais <aline.almeida@pontomais.com.br>
2022-08-03 16:37:45 -0300

    test: test the comment's controller

---
+++ spec/requests/comments_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+RSpec.describe "Comments", type: :request do
+  before(:each) do
+    @article_attributes = FactoryBot.attributes_for(:article)
+    post articles_path, params: { article: @article_attributes }
+    @article_id = Article.last.id
+  end
+
+  describe "GET /comments" do
+    before(:each) do
+      comments_url = "/articles/#{@article_id}/comments"
+      get comments_url
+    end
+
+    it "returns success status" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "returns the correct message for loaded comments" do
+      expect(response.body).to match(/Comentários carregados./)
+    end
+  end
+
+  describe "POST /comments" do
+    context "when it has all valid (optionals) parameters" do
+      before(:each) do
+        comments_url = "/articles/#{@article_id}/comments"
+        @comment_attributes = FactoryBot.attributes_for(:comment)
+        post comments_url, params: { comment: @comment_attributes }
+      end
+
+      it "creates the comment with the correct attributes" do
+        expect(Comment.last).to have_attributes(@comment_attributes)
+      end
+
+      it 'return the status code 200 - success' do
+        expect(response).to have_http_status(200)
+      end
+
+      it "returns the correct message for created article" do
+        expect(response.body).to match(/Comentário criado com sucesso./)
+      end
+    end
+  end
+
+  describe 'DELETE /comments' do
+    context 'when the comment exists' do
+
+      before(:each) do
+        comments_url = "/articles/#{@article_id}/comments"
+        @comment_attributes = FactoryBot.attributes_for(:comment)
+  
+        post comments_url, params: { comment: @comment_attributes }
+  
+        comment_id = Comment.last.id
+        delete "/#{comments_url}/#{comment_id}"
+      end
+
+      it 'returns status code 200 - success' do 
+        expect(response).to have_http_status(200)
+      end
+
+      it "returns the correct message for deleted comment" do
+        expect(response.body).to match(/Comentário excluído com sucesso./)
+      end
+
+      it 'destroy the record' do
+        comment = build(:comment)
+        expect{comment.reload}.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+  end
+end
